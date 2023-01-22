36ce510d5c22a8f2b016e26309548cf1a40f6faa
Jorge Nicolas Sosa <nicoomg96@gmail.com>
2023-01-21 23:50:15 -0300

    Mercado liebre 6

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.listen(3000, ()=>{
+    console.log("Servidor listo puerto 3000");
+})
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/register", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
